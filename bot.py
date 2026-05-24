ef8ae227d4e04b53933d463f458bc9b712b1d493
https://app-repair.com <tmonchak@gmail.com>
2026-05-24 18:08:06 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import asyncio
 import logging
 import random
 
-from aiogram import Bot, Dispatcher, types,
+from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
